bd6c5facd8525b504d5addc5a8d6211302542a76
Ernests <austrins.ernests@gmail.com>
2025-09-05 14:14:14 +0300

    Initial commit for Java SpringBoot

---
+++ src/main/java/com/kazenites/kazenites/KazenitesApplication.java
@@ -0,0 +1,11 @@
+package com.kazenites.kazenites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KazenitesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KazenitesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kazenites/kazenites/KazenitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazenites.kazenites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KazenitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
